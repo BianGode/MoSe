debd6072dc0841f5de2dc416bcacf1f1a997eddd
BianGode <bian2205@gmail.com>
2024-07-19 16:25:45 +0200

    added basic html structure and components/routes dir

--- client-side/src/App.jsx
+++ client-side/src/App.jsx
@@ -1,34 +1,38 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import {Outlet, useNavigate} from 'react-router-dom'
 import './App.css'
+import * as logoGreen from './assets/logos/logoGreen.jpg'
+import * as logoBlack from './assets/logos/logoBlack.jpg'
+import * as logoWhite from './assets/logos/logoWhite.jpg'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [theme, setTheme] = useState('')
+  const [logo, setLogo] = useState('')
+
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div>
+      <div className='selectTheme'>
+        <div className='box original active'></div>
+        <div className='box black'></div>
+        <div className='box white'></div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <header>
+      <img src={logo} alt="logo" />
+      {
+        // if the homepage active then not show the search icon in the header
+        // else do show
+      }
+      <div className='searchHeader'>
+        <img src="faSearch" alt="" />
+        <input type="text" />
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      </header>
+      <Outlet />
+      <footer></footer>
+    </div>
   )
 }
 
