10be5c6ce90a694162ef38514a62987fa9136b94
BianGode <bian2205@gmail.com>
2024-09-02 13:44:42 +0200

    added sidebar slide

--- client-side/src/App.jsx
+++ client-side/src/App.jsx
@@ -10,10 +10,7 @@ import {
 import "./assets/styles/App.css";
 import axios from "axios";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faBars } from "@fortawesome/free-solid-svg-icons";
-// import * as logoGreen from './assets/logos/logoGreen.jpg'
-// import * as logoBlack from './assets/logos/logoBlack.jpg'
-// import * as logoWhite from './assets/logos/logoWhite.jpg'
+import { faBars, faClose } from "@fortawesome/free-solid-svg-icons";
 
 function App() {
   const [theme, setTheme] = useState("");
@@ -31,9 +28,31 @@ function App() {
       setSearch(e.target.value);
     }
   }
-  
+
   function handleSidebarClick() {
-    const sidebar = document.querySelector('.sideBar')
+    const sidebar = document.querySelector(".sideBar");
+    const sidebarOpen = document.querySelector(".sidebar-open-icon");
+    const sidebarClose = document.querySelector(".sidebar-close-icon");
+    const sidebarLinks = document.querySelector(".sideBarLinks")
+
+    // sidebar slideshow
+    if (sidebarOpen.style.display == "flex" && sidebarLinks.style.display == "none") {
+      sidebar.style.animation = "sidebarSlideOpen 0.5s ease";
+      sidebar.style.display = "flex";
+      sidebarLinks.style.display = "block"
+      // setTimeout(() => {
+        sidebarOpen.style.display = "none";
+        sidebarClose.style.display = "flex";
+      // }, 500);
+    } else {
+      setTimeout(() => {
+        sidebar.style.display = "none";
+      }, 500);
+      sidebar.style.animation = "sidebarSlideClose 0.5s ease";
+      sidebarLinks.style.display = "none";
+      sidebarOpen.style.display = "flex";
+      sidebarClose.style.display = "none";
+    }
   }
 
   return (
@@ -43,37 +62,40 @@ function App() {
         <div className="box black"></div>
         <div className="box white"></div>
       </div>
-      {document.body.offsetWidth > 800 ? (
-        <header>
-          <img src={logo} alt="logo" />
-          {
-            // if the homepage active then not show the search icon in the header
-            // else do show
-          }
-          <div className="headerLinks">
+      <header className="desktop">
+        <img src={logo} alt="logo" />
+        {
+          // if the homepage active then not show the search icon in the header
+          // else do show
+        }
+        <div className="headerLinks">
+          <Link to="/about">About</Link>
+          <Link to="/home">Home</Link>
+        </div>
+        <div className="searchHeader">
+          <img src="faSearch" alt="" onClick={() => searchReqClientSide()} />
+          <input type="text" value={search} onKeyDown={(e) => checkInput(e)} />
+        </div>
+      </header>
+      <header className="mobile">
+        <FontAwesomeIcon
+          icon={faBars}
+          className="sidebar-open-icon"
+          onClick={() => handleSidebarClick()}
+        />
+        <div className="sideBar">
+          <FontAwesomeIcon
+            icon={faClose}
+            className="sidebar-close-icon"
+            onClick={() => handleSidebarClick()}
+          />
+          <div className="sideBarLinks">
             <Link to="/about">About</Link>
             <Link to="/home">Home</Link>
           </div>
-          <div className="searchHeader">
-            <img src="faSearch" alt="" onClick={() => searchReqClientSide()} />
-            <input
-              type="text"
-              value={search}
-              onKeyDown={(e) => checkInput(e)}
-            />
-          </div>
-        </header>
-      ) : (
-        <>
-          <FontAwesomeIcon icon={faBars} onClick={() => handleSidebarClick()} />
-          <div className="sideBar">
-            <div className="sideBarLinks">
-              <Link to="/about">About</Link>
-              <Link to="/home">Home</Link>
-            </div>
-          </div>
-        </>
-      )}
+        </div>
+      </header>
+
       <Outlet />
       <footer></footer>
     </div>
