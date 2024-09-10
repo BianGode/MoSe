a7d4192ae39d70c1d40bd868f962f4daefe97033
BianGode <bian2205@gmail.com>
2024-09-10 15:22:11 +0200

    videojs implementation and custom css

--- client-side/src/routes/Home.jsx
+++ client-side/src/routes/Home.jsx
@@ -1,15 +1,48 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import "../assets/styles/Home.css";
 import { faSearch } from "@fortawesome/free-solid-svg-icons";
+import VideoJS from "../components/VideoJS";
+import { useRef } from "react";
 
 function Home() {
+  const playerRef = useRef(null);
+
+  const videoJsOptions = {
+    autoplay: true,
+    controls: false,
+    muted: true,
+    loop: true,
+    // responsive: true,
+    fluid: false,
+    sources: [{
+      src: '../../public/header-movie.mp4',
+      type: 'video/mp4'
+    }]
+  };
+
+  const handlePlayerReady = (player) => {
+    playerRef.current = player;
+    
+    player.on('waiting', () => {
+      videojs.log('player is waiting')
+    })
+
+    player.on('dispose', () => {
+      videojs.log('player will dispose');
+    })
+  }
+
   return (
-    <div className="homeWrapper">
+    <div className="home-wrapper">
       <div className="search-wrap-home">
-        <div>
+        {/* <div>
           <FontAwesomeIcon icon={faSearch} />
         </div>
-        <div className="search-results"></div>
+        <div className="search-results"></div> */}
+      </div>
+      <div className="video-wrapper">
+        <VideoJS options={videoJsOptions} onReady={handlePlayerReady}/>
+        <div className="video-overlay"></div>
       </div>
     </div>
   );
